31ca2213a8de3b44dbc273df2afed4c0aad1ace3
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-02-12 21:34:53 -0800

    Add basic exec()

--- my_shell.cpp
+++ my_shell.cpp
@@ -112,10 +112,60 @@ Segment *parseSegments(char line[80]) {
   return first;
 }
 
+void redirectOut(int i, InputLine *input) {
+  FILE *fd;
+  int check_int;
+
+  if (i == 0) {
+    if (input->count == 1) {
+      //only arg
+      fprintf(stderr, ">: only arg\n");
+    } else {
+      //first arg
+      fprintf(stderr, ">: first arg\n");
+    }
+  } else if (i == input->count - 1) {
+    //last arg
+    fprintf(stderr, ">: last arg\n");
+    fprintf(stderr, "Missing name for redirect\n");
+  } else {
+    //MIDDLE ARG
+    fprintf(stderr, ">: middle arg\n");
+    if( access( input->args[i + 1], F_OK ) != -1 ) {
+      // file exists
+      fd = fopen(input->args[i + 1], "w");
+
+      fclose(fd);
+    } else {
+      // file doesn't exist
+      fprintf(stderr, "File %s does not exist or is invalid\n", input->args[i + 1]);
+    }
+
+
+  }
+}
+
+void redirectIn(int i, InputLine *input) {
+  if (i == 0) {
+    if (input->count == 1) {
+      //only arg
+      fprintf(stderr, "<: only arg\n");
+    } else {
+      //first arg
+      fprintf(stderr, "<: first arg\n");
+    }
+  } else if (i == input->count - 1) {
+    //last arg
+    fprintf(stderr, "<: last arg\n");
+  } else {
+    //middle arg
+    fprintf(stderr, "<: middle arg\n");
+  }
+}
+
 
 int main() {
   char line[80];
-
 
   while (fgets(line, 1000, stdin) != NULL) {
 
@@ -126,14 +176,25 @@ int main() {
 
     while (segment->next != NULL) {
       fprintf(stderr, "segment count = %d\n", segment->count);
-      for (int i = 0; i < segment->count; i++) {
-
+      for (int i = 0; i < input->count; i++) {
+        //redirection
+        if (strcmp(">", input->args[i]) == 0) {
+          redirectOut(i, input);
+        }
+        if (strcmp("<", input->args[i]) == 0) {
+          redirectIn(i, input);
+        }
       }
-
 
       segment = segment->next;
     }
     fprintf(stderr, "segment count = %d\n", segment->count);
+    switch(fork()) {
+      case 0:
+        execvp(segment->args[0], input->args);
+      default:
+        wait(NULL);
+    }
 
 
   }
